6c7e8ec2468b0f35483158303a656d35c84cbac4
ElizavetaRudko <eliz.vitebsk@gmail.com>
2020-05-08 15:55:18 +0300

    ecg report with js added, results by classes in %

--- cnn_code_4_classes/EcgApp/EcgApp/Controllers/ChooseEcgRecordController.cs
+++ cnn_code_4_classes/EcgApp/EcgApp/Controllers/ChooseEcgRecordController.cs
@@ -7,8 +7,9 @@ using System.Threading.Tasks;
 using EcgApp.ViewModels;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using System.Text.RegularExpressions;
+using System.Collections;
+using System.Security.Cryptography.Xml;
 
 namespace EcgApp.Controllers
 {
@@ -28,28 +29,28 @@ namespace EcgApp.Controllers
         }
 
         [HttpPost]
-        public string Ok(RecordViewModel model)
+        //public string Ok(RecordViewModel model)
+        public IActionResult Ok(RecordViewModel model)
+        //public async void Ok(RecordViewModel model)
         {
             string uniqueFileName = null;
+            string filePath = null;
 
             if (model.RecordFile != null)
             {
                 string uploadsFolder = Path.Combine(hostingEnvironment.WebRootPath, "records");
                 uniqueFileName = Guid.NewGuid().ToString() + "_" + model.RecordFile.FileName;
-                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                filePath = Path.Combine(uploadsFolder, uniqueFileName);
                 model.RecordFile.CopyTo(new FileStream(filePath, FileMode.Create));
             }
 
             //execute .py
             // create process info
             var psi = new ProcessStartInfo();
-            //?? такой ли путь
-            //psi.FileName = @"C:\Users\User\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.7";
             psi.FileName = @"C:\Users\Professional\AppData\Local\Programs\Python\Python36\python.exe";
-            //psi.FileName = @"C: \Users\User\AppData\Local\Programs\Python\Python37 - 32\python.exe";
-            //psi.FileName = @"C: \Users\User\AppData\Local\Programs\Python\Python37 - 32\python.exe";
-        //provide script and argument
-            var script = @"C:\Users\Professional\Desktop\1studing\CourseWork3\course_work_app\cnn_code_4_classes\predicting_4.py";
+
+            //provide script and argument
+            var script = @"C:\Users\Professional\Desktop\1studing\CourseWork3\course_work_app\Course_work\cnn_code_4_classes\predicting_4.py";
             var arg = model.RecordFile.FileName.Split(".")[0];
 
             psi.Arguments = $"{script} {arg}";
@@ -64,7 +65,7 @@ namespace EcgApp.Controllers
             var errors = "";
             string results = "";
 
-            using(var process = Process.Start(psi))
+            using (var process = Process.Start(psi))
             {
                 errors = process.StandardError.ReadToEnd();
                 results = process.StandardOutput.ReadToEnd();
@@ -72,10 +73,48 @@ namespace EcgApp.Controllers
             }
 
             //display output
-            //return "ok";
-            return arg + " " + results;
+            //return arg + " " + results;
 
-            //return View();
+            string data = null;
+            using (var reader = new StreamReader(model.RecordFile.OpenReadStream()))
+            {
+                while (reader.Peek() >= 0)
+                    data += reader.ReadLine();
+                //data = reader.ReadToEnd();
+            }
+
+            //var listValues = Array.ConvertAll(data.Split(' ', '\n', '\r'), int.Parse);
+
+            List<int> listValues = new List<int>();
+
+            string[] numbers = Regex.Split(data, @"\D+");
+            foreach (string value in numbers)
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    int i = int.Parse(value);
+                    listValues.Add(i);
+                }
+            }
+
+            ViewBag.VALUES = listValues;
+            ViewBag.RESULT = results;
+
+            string[] allResults = results.Split(' ');
+            //string resN = "";
+            //resN = results.Substring(2, 1);
+            string resN = allResults[2] + "%";
+            string resRBBB = allResults[4] + "%";
+            string resB = allResults[6] + "%";
+            string resAFIB = allResults[8] + "%";
+
+            ViewBag.N = resN;
+            ViewBag.RBBB = resRBBB;
+            ViewBag.B = resB;
+            ViewBag.AFIB = resAFIB;
+            //+ передавать в виде строки или файла массив начал, концов и типов патологий для выделения цветом на графике
+
+            return View();
 
         }
     }
